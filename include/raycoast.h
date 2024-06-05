f45b22d071b5161115ba0d37c76e0c346681e2b1
brotheringbullshit <166644732+brotheringbullshit@users.noreply.github.com>
2024-06-05 16:28:36 -0300

    Update raycoast.h

--- include/raycoast.h
+++ include/raycoast.h
@@ -3,7 +3,6 @@
 
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -16,11 +15,8 @@
 #define MOVE_SPEED 2.0
 #define ROTATE_SPEED 2.0
 
-// Structure to hold wall information
 typedef struct {
-    bool isTexture;        // Whether the wall uses a texture or a solid color
-    SDL_Texture *texture;  // Texture to use for the wall (if isTexture is true)
-    Uint8 r, g, b;         // RGB color values for the wall (if isTexture is false)
+    Uint8 r, g, b; // RGB color values for the wall
 } Wall;
 
 typedef struct {
@@ -32,10 +28,8 @@ typedef struct {
 bool initializeSDL();
 void closeSDL();
 void drawRect(int x, int y, int width, int height, Uint8 r, Uint8 g, Uint8 b);
-void drawTexture(SDL_Texture *texture, int x, int y, int width, int height);
 void castRays(Player *player, Wall walls[MAP_HEIGHT][MAP_WIDTH]);
 void handleInput(Player *player, bool *quit);
 void renderFrame(Player *player, Wall walls[MAP_HEIGHT][MAP_WIDTH]);
-SDL_Texture *loadTexture(const char *file);
 
 #endif // RAYCOAST_H
