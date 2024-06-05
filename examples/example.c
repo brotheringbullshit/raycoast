e384acc2a29866e7e056555bf624e0555ac9045e
brotheringbullshit <166644732+brotheringbullshit@users.noreply.github.com>
2024-06-05 16:29:57 -0300

    example code

---
+++ examples/example.c
@@ -0,0 +1,40 @@
+// this is just used for whenever you installed it or something
+
+#include "raycoast.h"
+
+int map[MAP_HEIGHT][MAP_WIDTH] = {
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+int main(int argc, char *argv[]) {
+    if (!initializeSDL()) {
+        return 1;
+    }
+
+    Player player = {150, 150, 0};
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        handleInput(&player, &quit);
+
+        // Render frame
+        renderFrame(&player, map);
+    }
+
+    closeSDL();
+    return 0;
+}
